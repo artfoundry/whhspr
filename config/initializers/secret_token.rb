d25494b222c28b849bf39882a2bc53680ac32b6e
Apprentice <apprentice@dbc-22.local>
2013-10-07 15:03:59 -0700

    first god commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whhspr::Application.config.secret_token = '14328140de31b412b22fce1c34e7e953edb1682ec8f2b1acdb01a40eaf8c868ff7716d8af8fe6bc2115b992df0368cdac296165509ce994eec96a83136212676'
