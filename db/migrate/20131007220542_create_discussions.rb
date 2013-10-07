1a57e47584650871cca36e604a96cc906811dff0
Apprentice <apprentice@dbc-22.local>
2013-10-07 15:11:26 -0700

    writes discussion migration

--- db/migrate/20131007220542_create_discussions.rb
+++ db/migrate/20131007220542_create_discussions.rb
@@ -1,7 +1,13 @@
 class CreateDiscussions < ActiveRecord::Migration
   def up
+    create_table :discussions do |t|
+      t.string :body
+      t.string :title
+      t.timestamps
+    end
   end
 
   def down
+    drop_table :discussions
   end
 end
