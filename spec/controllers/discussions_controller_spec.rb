4a09a01486d2efca1964ad651257077b0d329ad0
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-08 11:21:53 -0700

    adds controller unit tests for discussion

--- spec/controllers/discussions_controller_spec.rb
+++ spec/controllers/discussions_controller_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe DiscussionsController do
   describe "GET index" do
-    it "assigns @discussions" do
+    it "@discussions should have all discussions" do
+      5.times {FactoryGirl.create(:discussion)}
       get :index
-      expect(assigns(:discussions)).to eq(Discussion.all)
+      expect(assigns(:discussions).count).to eq(5)
+      expect(assigns(:discussions)[0]).to be_a(Discussion)
     end
   end
+
+  describe "GET show" do
+    let(:discussion) {FactoryGirl.create(:discussion)}
+
+    it "finds @discussion" do
+      get :show, :id => discussion.id
+      expect(assigns(:discussion)).to eq(discussion)
+    end
+  end
+
 end
