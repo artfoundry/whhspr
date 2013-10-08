aa5125ed1904d5b3b4246681975ca6ff38b9eadd
Apprentice <apprentice@dbc-22.local>
2013-10-08 15:28:48 -0700

    adds feature test: user submits new discussion

--- spec/features/discussions_spec.rb
+++ spec/features/discussions_spec.rb
@@ -34,4 +34,17 @@ describe "discussions request" do
 
 end
 
+feature "discussions" do
 
+  let(:discussion) { FactoryGirl.create(:discussion) }
+
+  scenario "User creates a new discussion" do
+    visit discussions_path
+    fill_in "Title", :with => discussion.title
+    fill_in "Body", :with => discussion.body
+
+    click_button "Create Discussion"
+
+    expect(page).to have_text(discussion.title)
+  end
+end
